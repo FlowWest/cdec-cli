af4b13450376ee5047b90f4d8ff3fd07edc77664
ergz <emnlrodriguez@gmail.com>
2024-02-24 08:19:19 -0800

    trying to find the html table in the station metadata page

--- cmd/cdec-cli/main.go
+++ cmd/cdec-cli/main.go
@@ -8,11 +8,18 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+
+	"golang.org/x/net/html"
 )
 
 var cdecUrls = map[string]string{
 	"query":    "https://cdec.water.ca.gov/dynamicapp/req/JSONDataServlet",
 	"stations": "https://cdec.water.ca.gov/dynamicapp/staMeta",
+}
+
+type cliCmd struct {
+	cmd  string
+	args []string
 }
 
 type queryOptions struct {
@@ -21,6 +28,21 @@ type queryOptions struct {
 	dur_code  string
 	startDate string
 	endDate   string
+}
+
+// For sure overkill but just to stay consistent
+type stationOptions struct {
+	stationId string
+}
+
+func findTable(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "table" {
+		fmt.Println("Found table tag:", n.Data)
+		return
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		findTable(c)
+	}
 }
 
 func main() {
@@ -35,12 +57,16 @@ func main() {
 		fmt.Println("Usage: cdec-cli <command> [arguments]")
 		os.Exit(1)
 	}
-	var options queryOptions
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	switch os.Args[1] {
 	case "query":
 		{
+			var options queryOptions
+			// set the query command
 			queryCommand := flag.NewFlagSet("query", flag.ExitOnError)
+
+			// append to this base command arguments needed to query the data
 			queryCommand.StringVar(&options.station, "station", "", "Station ID to query data for. Use 'cdec-cli station --list' for a complete list of available stations")
 			queryCommand.StringVar(&options.sensor, "sensor", "", "Sensor number to query data for")
 			queryCommand.StringVar(&options.dur_code, "duration", "", "duration code for data, can be one of (d)aily, (h)ourly, or (m)onthly")
@@ -55,8 +81,6 @@ func main() {
 			params.Set("dur_code", options.dur_code)
 			params.Set("Start", options.startDate)
 			params.Set("End", options.endDate)
-
-			logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 			u, err := url.Parse(cdecUrls["query"])
 			if err != nil {
@@ -75,7 +99,7 @@ func main() {
 
 			body, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
-				fmt.Println("Error reading response body:", err)
+				logger.Println("Error reading response body:", err)
 				return
 			}
 
@@ -83,7 +107,39 @@ func main() {
 		}
 	case "stations":
 		{
-			fmt.Println("yet to be implemented!")
+			var stationOptions stationOptions
+			stationCommand := flag.NewFlagSet("station", flag.ExitOnError)
+
+			stationCommand.StringVar(&stationOptions.stationId, "stationID", "", "Station ID to retrieva metadata for")
+
+			stationCommand.Parse(os.Args[2:])
+
+			params := url.Values{}
+			params.Set("station_id", stationOptions.stationId)
+
+			// build the url for stations
+			u, err := url.Parse(cdecUrls["stations"])
+			if err != nil {
+				logger.Println("error trying to parse url")
+				return
+			}
+
+			u.RawQuery = params.Encode()
+
+			resp, err := http.Get(u.String())
+			if err != nil {
+				logger.Println("error trying to reach the statiosn endpoint")
+				return
+			}
+
+			doc, err := html.Parse(resp.Body)
+			if err != nil {
+				logger.Println("error trying to parse the body")
+				return
+			}
+
+			findTable(doc)
+			logger.Printf("you are trying to get info for %s", stationOptions.stationId)
 		}
 	}
 	flag.Parse()
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ergz/cdec-cli
 
 go 1.21.0
+
+require golang.org/x/net v0.21.0
